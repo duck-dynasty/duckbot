17bdbd131c1bf071b88b72d6e8aaa8a62768cd3b
Taras Mychaskiw <twentylemon@users.noreply.github.com>
2020-12-10 23:11:53 -0500

    first crack at tests (#15)

--- tests/test_tools.py
+++
@@ -1,8 +0,0 @@
-import duckbot.tools as tools 
-import pytest
-
-def test_kubernetes_correction():
-    test_message = tools.get_correction("tom", "kubernetes")
-
-    assert test_message == "I think tom means K8s"
-# end def test_kubernetes_correction
