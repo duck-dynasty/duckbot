ea6b21f050b8a0b8c039a8397bb469a941846d44
Taras Mychaskiw <tarasmy@amazon.com>
2020-12-18 11:07:45 -0500

    address empty implementation code smell

--- tests/async_mock_ext.py
+++ tests/async_mock_ext.py
@@ -4,6 +4,7 @@ import mock
 def patch_async_mock(func):
     """A decorator to allow @mock.patch to work for async methods"""
     async def async_magic():
+        """This method's __await__ is used to hack mock awaits. No implementation needed."""
         pass
 
     async def async_wrap(*args, **kwargs):
