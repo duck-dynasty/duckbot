e3a4be7a4e3552f2c347052fe59bd38115dfc049
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2021-02-20 17:18:16 -0500

    finish implementation, add tests

--- duckbot/cogs/insights.py
+++ duckbot/cogs/insights.py
@@ -1,4 +1,7 @@
 import random
+import datetime
+import pytz
+import server.channels as channels
 from discord.ext import commands, tasks
 
 responses = [
@@ -19,14 +22,26 @@ class Insights(commands.Cog):
     def cog_unload(self):
         self.check_should_respond.cancel()
 
-    @tasks.loop(minutes=29.0)
+    @tasks.loop(minutes=139.0)
     async def check_should_respond(self):
         await self.__check_should_respond()
     async def __check_should_respond(self):
-        # get the most recent message in every channel
-        # if it was dave and more than 23 minutes old
-          # respond with a random response
-        pass
+        channel = self.bot.get_channel(channels.GENERAL)
+        message = await self.__get_last_message(channel)
+        if self.should_respond(message):
+            response = random.choice(responses)
+            await channel.send(response)
+
+    async def __get_last_message(self, channel):
+        message = await channel.history(limit = 1).flatten()
+        if message:
+            return message[0]
+        else:
+            return None
+    
+    def should_respond(self, message):
+        stamp = datetime.datetime.utcnow() - datetime.timedelta(minutes=23)
+        return message != None and stamp >= message.created_at and message.author.id == 244629273191645184
 
     @check_should_respond.before_loop
     async def before_loop(self):
---
+++ tests/cogs/test_insights.py
@@ -0,0 +1,83 @@
+import pytest
+import mock
+import datetime
+from async_mock_ext import patch_async_mock
+import server.channels as channels
+from cogs.insights import Insights
+
+@pytest.mark.asyncio
+@patch_async_mock
+@mock.patch('discord.ext.commands.Bot')
+@mock.patch('discord.TextChannel')
+async def test_check_should_respond_no_messages(bot, channel):
+    bot.get_channel.return_value = channel
+    channel.history.return_value = Hist(None)
+    clazz = Insights(bot, start_tasks=False)
+    await clazz._Insights__check_should_respond()
+    bot.get_channel.assert_called_once_with(channels.GENERAL)
+    channel.send.assert_not_called()
+
+@pytest.mark.asyncio
+@patch_async_mock
+@mock.patch('discord.ext.commands.Bot')
+@mock.patch('discord.TextChannel')
+@mock.patch('discord.Message')
+async def test_check_should_respond_new_message(bot, channel, message):
+    dt = mock.Mock(wraps = datetime.datetime)
+    dt.utcnow.return_value = datetime.datetime(2000, 1, 1, hour = 12, minute = 00)
+    with mock.patch('datetime.datetime', new = dt):
+        bot.get_channel.return_value = channel
+        message.created_at = datetime.datetime(2000, 1, 1, hour = 11, minute = 38)
+        message.author.id = 244629273191645184
+        channel.history.return_value = Hist(message)
+        clazz = Insights(bot, start_tasks=False)
+        await clazz._Insights__check_should_respond()
+        bot.get_channel.assert_called_once_with(channels.GENERAL)
+        channel.send.assert_not_called()
+
+@pytest.mark.asyncio
+@patch_async_mock
+@mock.patch('discord.ext.commands.Bot')
+@mock.patch('discord.TextChannel')
+@mock.patch('discord.Message')
+async def test_check_should_respond_not_special_user(bot, channel, message):
+    dt = mock.Mock(wraps = datetime.datetime)
+    dt.utcnow.return_value = datetime.datetime(2000, 1, 1, hour = 12, minute = 00)
+    with mock.patch('datetime.datetime', new = dt):
+        bot.get_channel.return_value = channel
+        message.created_at = datetime.datetime(2000, 1, 1, hour = 11, minute = 00)
+        message.author.id = 0
+        channel.history.return_value = Hist(message)
+        clazz = Insights(bot, start_tasks=False)
+        await clazz._Insights__check_should_respond()
+        bot.get_channel.assert_called_once_with(channels.GENERAL)
+        channel.send.assert_not_called()
+
+@pytest.mark.asyncio
+@patch_async_mock
+@mock.patch('discord.ext.commands.Bot')
+@mock.patch('discord.TextChannel')
+@mock.patch('discord.Message')
+async def test_check_should_respond_old_message_sent_by_special_user(bot, channel, message):
+    dt = mock.Mock(wraps = datetime.datetime)
+    dt.utcnow.return_value = datetime.datetime(2000, 1, 1, hour = 12, minute = 00)
+    with mock.patch('datetime.datetime', new = dt):
+        bot.get_channel.return_value = channel
+        message.created_at = datetime.datetime(2000, 1, 1, hour = 11, minute = 00)
+        message.author.id = 244629273191645184
+        channel.history.return_value = Hist(message)
+        clazz = Insights(bot, start_tasks=False)
+        await clazz._Insights__check_should_respond()
+        bot.get_channel.assert_called_once_with(channels.GENERAL)
+        channel.send.assert_called()
+
+class Hist:
+    def __init__(self, message):
+        self.message = message
+    
+    async def flatten(self):
+        if self.message:
+            return [ self.message ]
+        else:
+            return []
+    
