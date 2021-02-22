d09bcbf934ace383d6efecff8bf5cfe724fcf65e
Taras Mychaskiw <tarasmy@amazon.com>
2021-02-22 12:21:48 -0500

    react with yugoslavian flags when tito is added as reaction

--- tests/async_mock_ext.py
+++ tests/async_mock_ext.py
@@ -11,3 +11,6 @@ def patch_async_mock(func):
         mock.MagicMock.__await__ = lambda x: async_magic().__await__()
         await func(*args, **kwargs)
     return async_wrap
+
+async def async_value(value):
+    return value
--- tests/cogs/test_tito.py
+++ tests/cogs/test_tito.py
@@ -1,6 +1,6 @@
 import pytest
 import mock
-from async_mock_ext import patch_async_mock
+from async_mock_ext import patch_async_mock, async_value
 from cogs.tito import Tito
 
 @pytest.mark.asyncio
@@ -32,6 +32,35 @@ async def test_react_to_tito_with_yugoslavia_message_contains_tito_emoji(message
     msg = await clazz.react_to_tito_with_yugoslavia(message)
     assert msg == None
     assert_flags_sent(message)
+
+@pytest.mark.asyncio
+@patch_async_mock
+@mock.patch('discord.ext.commands.Bot')
+@mock.patch('discord.RawReactionActionEvent')
+async def test_react_to_tito_reaction_no_tito_emoji(bot, payload):
+    payload.emoji.name = "not-tito"
+    clazz = Tito(bot)
+    await clazz.react_to_tito_reaction(payload)
+    bot.fetch_channel.assert_not_called()
+
+@pytest.mark.asyncio
+@patch_async_mock
+@mock.patch('discord.ext.commands.Bot')
+@mock.patch('discord.RawReactionActionEvent')
+@mock.patch('discord.TextChannel')
+@mock.patch('discord.Message')
+async def test_react_to_tito_reaction_tito_emoji(bot, payload, channel, message):
+    payload.channel_id = 123
+    payload.message_id = 456
+    payload.emoji.name = "tito"
+    bot.fetch_channel.return_value = async_value(channel)
+    channel.fetch_message.return_value = async_value(message)
+    clazz = Tito(bot)
+    await clazz.react_to_tito_reaction(payload)
+    bot.fetch_channel.assert_called_once_with(payload.channel_id)
+    channel.fetch_message.assert_called_once_with(payload.message_id)
+    assert_flags_sent(message)
+
 
 def assert_flags_sent(message):
     calls = [
