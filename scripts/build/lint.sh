b221aa39a5d97508a29064f6ad4acefa2c628b51
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2021-03-15 19:29:03 -0400

    add black to lint check

--- scripts/build/lint.sh
+++ scripts/build/lint.sh
@@ -1,9 +1,10 @@
 . $(git rev-parse --show-toplevel)/scripts/ensure-toplevel.sh && \
 
 # stop the build if there are Python syntax errors or undefined names
-# python3.8 -m flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics && \
+python3.8 -m flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics && \
 
 # exit-zero treats all errors as warnings. The GitHub editor is 127 chars wide
-# python3.8 -m flake8 . --count --exit-zero --max-complexity=10 --max-line-length=200 --statistics
+python3.8 -m flake8 . --count --exit-zero --max-complexity=10 --max-line-length=200 --statistics && \
 
-python3.8 -m black --line-length 200 duckbot tests
+# check if the formatter ran; change format.sh if this is changed!
+python3.8 -m black --line-length 200 --diff --check duckbot tests
