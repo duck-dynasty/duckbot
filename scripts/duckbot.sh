25c37bd1531669192ada37c01c65b9811a87698b
Taras Mychaskiw <tarasmy@amazon.com>
2021-03-15 13:35:50 -0400

    change TOKEN to DISCORD_TOKEN

--- scripts/duckbot.sh
+++ scripts/duckbot.sh
@@ -1,9 +1,9 @@
 . $(git rev-parse --show-toplevel)/scripts/ensure-toplevel.sh && \
 
 # check for .env
-if stat duckbot/.env > /dev/null 2>&1 && grep -q TOKEN duckbot/.env; then
+if stat duckbot/.env > /dev/null 2>&1 && grep -q DISCORD_TOKEN duckbot/.env; then
     # run duckbot, brother
     python3.8 duckbot/main.py
 else
-    echo "No .env file or token not set. Yell at Tom to get the token."
+    echo "No .env file or DISCORD_TOKEN not set. Yell at Tom to get the token."
 fi
