bb27fe5d4aa1cc7a78b6a01a1878878d611e70a5
Taras Mychaskiw <twentylemon@users.noreply.github.com>
2021-03-16 09:09:24 -0400

    change TOKEN to DISCORD_TOKEN (#93)

--- scripts/duckbot.sh
+++ scripts/duckbot.sh
@@ -1,9 +1,9 @@
 . $(git rev-parse --show-toplevel)/scripts/ensure-toplevel.sh && \
 
 # check for .env
-if stat duckbot/.env > /dev/null 2>&1 && grep -q TOKEN duckbot/.env; then
+if stat duckbot/.env > /dev/null 2>&1 && grep -q DISCORD_TOKEN duckbot/.env; then
     # run duckbot, brother, for a max of 1h
     timeout 1h python3.8 duckbot/main.py || echo "we killed your idle duckbot"
 else
-    echo "No .env file or token not set. Yell at Tom to get the token."
+    echo "No .env file or DISCORD_TOKEN not set. Yell at Tom to get the token."
 fi
