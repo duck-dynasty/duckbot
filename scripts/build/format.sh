7b41a12ec240a50e6e6b7711e02f6c745c0ec7f1
Taras Mychaskiw <tarasmy@amazon.com>
2021-03-16 09:11:41 -0400

    use pyproject to configure black

--- scripts/build/format.sh
+++ scripts/build/format.sh
@@ -1,4 +1,4 @@
 . $(git rev-parse --show-toplevel)/scripts/ensure-toplevel.sh && \
 
-# change lint.sh if this is changed!
-python3.8 -m black --line-length 200 duckbot tests
+# run black formatter on the code
+python3.8 -m black duckbot tests
--- scripts/build/lint.sh
+++ scripts/build/lint.sh
@@ -6,5 +6,5 @@ python3.8 -m flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics 
 # exit-zero treats all errors as warnings. The GitHub editor is 127 chars wide
 python3.8 -m flake8 . --count --exit-zero --max-complexity=10 --max-line-length=200 --statistics && \
 
-# check if the formatter ran; change format.sh if this is changed!
-python3.8 -m black --line-length 200 --diff --check duckbot tests
+# check if the format is correct
+python3.8 -m black --diff --check duckbot tests
