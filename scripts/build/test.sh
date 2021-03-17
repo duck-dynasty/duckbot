2f29bc189d601a6d3ed4c858c5b006cba57f5052
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2021-03-16 20:22:25 -0400

    generate html coverage report

--- scripts/build/test.sh
+++ scripts/build/test.sh
@@ -4,4 +4,7 @@
 python3.8 -m coverage run --source=duckbot/ -m pytest -s && \
 
 # when tests pass, output coverage report
-python3.8 -m coverage report --show-missing --skip-empty --skip-covered --fail-under=80
+python3.8 -m coverage report --show-missing --skip-empty --skip-covered --fail-under=80 && \
+
+# generate html coverage report
+python3.8 -m coverage html && echo "html report: file://$(pwd)/htmlcov/index.html"
