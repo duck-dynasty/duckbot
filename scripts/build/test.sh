2ad31b42c67811806f58705bdfe1236bd2f0e678
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2021-03-16 20:14:22 -0400

    run coverage tests

--- scripts/build/test.sh
+++ scripts/build/test.sh
@@ -1,4 +1,7 @@
 . $(git rev-parse --show-toplevel)/scripts/ensure-toplevel.sh && \
 
-# test with pytest
-python3.8 -m pytest -s
+# test with pytest and gather code coverage
+python3.8 -m coverage run --source=duckbot/ -m pytest -s && \
+
+# when tests pass, output coverage report
+python3.8 -m coverage report --show-missing --skip-empty --skip-covered --fail-under=80
