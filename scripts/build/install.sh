89c96eac88221cb95d93ee9b44c7f79fbfc3e19d
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2021-03-22 22:09:44 -0400

    install ffmpeg in install script

--- scripts/build/install.sh
+++ scripts/build/install.sh
@@ -1,4 +1,6 @@
 # install package requirements
 python3.8 -m pip install -r "$(git rev-parse --show-toplevel)/requirements.txt" && \
 # install test and build dependencies
-python3.8 -m pip install -r "$(git rev-parse --show-toplevel)/requirements-dev.txt"
+python3.8 -m pip install -r "$(git rev-parse --show-toplevel)/requirements-dev.txt" && \
+# install apt dependencies
+sudo apt-get install -y ffmpeg
