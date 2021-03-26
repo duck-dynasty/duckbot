4f22082995360720edb57bcc3d6abc165d86a020
Taras Mychaskiw <tarasmy@amazon.com>
2021-03-26 15:47:20 -0400

    wip

--- scripts/build/install.sh
+++ scripts/build/install.sh
@@ -1,4 +1,9 @@
-# install package requirements
+. "$(git rev-parse --show-toplevel)/env/prod/bin/activate" && \
 python3.8 -m pip install -r "$(git rev-parse --show-toplevel)/requirements.txt" && \
-# install test and build dependencies
-python3.8 -m pip install -r "$(git rev-parse --show-toplevel)/requirements-dev.txt"
+deactivate && \
+
+# activate test env
+. "$(git rev-parse --show-toplevel)/env/dev/bin/activate" && \
+python3.8 -m pip install -r "$(git rev-parse --show-toplevel)/requirements.txt" && \
+python3.8 -m pip install -r "$(git rev-parse --show-toplevel)/requirements-dev.txt" && \
+deactivate
---
+++ scripts/build/venv.sh
@@ -0,0 +1,12 @@
+if [[ -z "$1" || "$1" = "build" ]]; then
+    rm -rf "$(git rev-parse --show-toplevel)/env/" && \
+    rm -rf "$(git rev-parse --show-toplevel)/ENV/" && \
+    rm -rf "$(git rev-parse --show-toplevel)/venv/" && \
+    rm -rf "$(git rev-parse --show-toplevel)/VENV/" && \
+    python3.8 -m venv "$(git rev-parse --show-toplevel)/env/prod" && \
+    python3.8 -m venv "$(git rev-parse --show-toplevel)/env/dev"
+elif [[ "$1" = "deactivate" ]]; then
+    deactivate
+else
+    . "$(git rev-parse --show-toplevel)/env/$1/bin/activate"
+fi
