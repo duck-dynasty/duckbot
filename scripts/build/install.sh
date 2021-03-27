87165ee0a5c63e72c1c2353f88295923f40f7dba
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2021-03-27 16:44:31 -0400

    minimize changeset

--- scripts/build/install.sh
+++ scripts/build/install.sh
@@ -1,8 +1,4 @@
 # install package requirements
-if [[ -z "$1" || "$1" = "prod" ]]; then
-    python3.8 -m pip install -r "$(git rev-parse --show-toplevel)/requirements.txt"
-fi && \
+python3.8 -m pip install -r "$(git rev-parse --show-toplevel)/requirements.txt" && \
 # install test and build dependencies
-if [[ -z "$1" || "$1" = "dev" ]]; then
-    python3.8 -m pip install -r "$(git rev-parse --show-toplevel)/requirements-dev.txt"
-fi
+python3.8 -m pip install -r "$(git rev-parse --show-toplevel)/requirements-dev.txt"
