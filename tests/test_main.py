836ad0c482fa818d91d8a75967a5da09f4bd0c38
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2021-03-28 16:21:58 -0400

    add main tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,2 +1,16 @@
-# a do nothing file for coverage sake
-import duckbot.__main__
+import sys
+import mock
+from duckbot.__main__ import duckbot
+
+
+@mock.patch("discord.ext.commands.Bot")
+def test_duckbot_dryrun(bot):
+    with mock.patch.object(sys, "argv", ["dry-run"]):
+        duckbot(bot)
+        bot.run.assert_not_called()
+
+
+@mock.patch("discord.ext.commands.Bot")
+def test_duckbot_normal_run(bot):
+    duckbot(bot)
+    bot.run.assert_called()
