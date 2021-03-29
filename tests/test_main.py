82118dfd1af3f829eac015cbed0cdfd757071828
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2021-03-29 10:20:00 -0400

    fix test warnings

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,13 +4,15 @@ from duckbot.__main__ import duckbot
 
 
 @mock.patch("discord.ext.commands.Bot")
-def test_duckbot_dryrun(bot):
+@mock.patch("discord.ext.tasks.Loop")
+def test_duckbot_dryrun(bot, loop):
     with mock.patch.object(sys, "argv", ["dry-run"]):
         duckbot(bot)
         bot.run.assert_not_called()
 
 
 @mock.patch("discord.ext.commands.Bot")
-def test_duckbot_normal_run(bot):
+@mock.patch("discord.ext.tasks.Loop")
+def test_duckbot_normal_run(bot, loop):
     duckbot(bot)
     bot.run.assert_called()
