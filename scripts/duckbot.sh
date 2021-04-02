aa82f48f077798bc4a227572408300698515e9a7
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2021-04-02 17:46:53 -0400

    remove notes

--- scripts/duckbot.sh
+++ scripts/duckbot.sh
@@ -8,9 +8,3 @@ if stat duckbot/.env > /dev/null 2>&1 && grep -q DISCORD_TOKEN duckbot/.env; the
 else
     echo "No .env file or DISCORD_TOKEN not set. Yell at Tom to get the token."
 fi
-
-# TODO deploy becomes
-# docker run -d -p 5000:5000 --restart=always --name registry registry:2
-# docker-compose build
-# docker-compose push
-# docker stack deploy --compose-file docker-compose.yml duckbot
