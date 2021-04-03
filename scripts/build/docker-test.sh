0f8ed585c606204876a4b6af70c505eee05149f8
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2021-04-03 12:44:11 -0400

    remove docker test

--- scripts/build/docker-test.sh
+++
@@ -1,6 +0,0 @@
-# runs a containerized install/build/test
-. $(git rev-parse --show-toplevel)/scripts/ensure-toplevel.sh && \
-
-docker build . --file scripts/build/Dockerfile --tag duckbot-test:latest
-
-docker rmi duckbot-test:latest
