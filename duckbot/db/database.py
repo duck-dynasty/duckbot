7a57d4f36501ade54ecf185ef22d1c2f277f7745
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2021-04-03 21:10:03 -0400

    trigger deploy

--- duckbot/db/database.py
+++ duckbot/db/database.py
@@ -11,7 +11,7 @@ class Database(commands.Cog, name="db"):
     async def test_command(self, context):
         with self.db.connect() as conn:
             conn.execute(text("CREATE TABLE IF NOT EXISTS some_table (x int, y int)"))
-            conn.execute(text("INSERT INTO some_table (x, y) VALUES (:x, :y)"), [{"x": 1, "y": 1}, {"x": 2, "y": 4}])
+            # conn.execute(text("INSERT INTO some_table (x, y) VALUES (:x, :y)"), [{"x": 1, "y": 1}, {"x": 2, "y": 4}])
             result = conn.execute(text("SELECT x, y FROM some_table"))
             for row in result:
                 print(f"x: {row.x}  y: {row.y}")
