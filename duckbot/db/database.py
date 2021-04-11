3ba453844d371d4913e96c6c7bc2a3d08430eb5d
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2021-04-11 14:44:00 -0400

    test database

--- duckbot/db/database.py
+++ duckbot/db/database.py
@@ -6,15 +6,7 @@ from sqlalchemy.orm import sessionmaker
 class Database(commands.Cog, name="db"):
     def __init__(self, bot):
         self.bot = bot
-        self.db = create_engine("postgresql+psycopg2://duckbot:pond@postgres/duckbot", echo=True, pool_pre_ping=True)
-
-    @commands.command(name="db")
-    async def test_command(self, context):
-        with self.db.connect() as conn:
-            result = conn.execute(text("SELECT * FROM weather_locations"))
-            for row in result:
-                print(f"{row}")
-                await context.send(f"{row}")
+        self.db = create_engine("postgresql+psycopg2://duckbot:pond@postgres/duckbot", pool_pre_ping=True)
 
     def session(self, item_type):
         item_type.metadata.create_all(self.db)
