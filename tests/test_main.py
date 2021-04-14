01e4b89e1605871c43b76fab78e5d2aad0e2382f
Taras Mychaskiw <tarasmy@amazon.com>
2021-04-14 14:27:52 -0400

    use in tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,7 @@
 import sys
 import mock
 from unittest.mock import call
+from tests.discord_test_ext import assert_cog_added_of_type
 from duckbot.__main__ import duckbot
 from duckbot.util import ConnectionTest
 
@@ -10,7 +11,7 @@ from duckbot.util import ConnectionTest
 def test_duckbot_connection_test(bot, loop):
     with mock.patch.object(sys, "argv", ["connection-test"]):
         duckbot(bot)
-        assert_cog_added(bot, ConnectionTest)
+        assert_cog_added_of_type(bot, ConnectionTest)
         bot.run.assert_called()
 
 
@@ -19,10 +20,3 @@ def test_duckbot_connection_test(bot, loop):
 def test_duckbot_normal_run(bot, loop):
     duckbot(bot)
     bot.run.assert_called()
-
-
-def assert_cog_added(bot, typ):
-    for invocation in bot.add_cog.call_args_list:
-        if isinstance(invocation[0], typ):
-            return True
-    return False
