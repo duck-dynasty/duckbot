5287e23bfb9b0cd8ffcaafd94e6861192fa331a0
Taras Mychaskiw <twentylemon@users.noreply.github.com>
2021-04-28 11:47:22 -0400

    move fortune into an extension (#161)

--- duckbot/__main__.py
+++ duckbot/__main__.py
@@ -4,13 +4,14 @@ from discord.ext import commands
 import duckbot.cogs.tito
 import duckbot.cogs.robot
 import duckbot.cogs.audio
+import duckbot.cogs.fortune
 import duckbot.cogs.recipe
 import duckbot.cogs.weather
 import duckbot.cogs.messages
 import duckbot.cogs.insights
 import duckbot.cogs.formula_one
 import duckbot.cogs.announce_day
-from duckbot.cogs import Duck, Typos, Bitcoin, Kubernetes, Fortune
+from duckbot.cogs import Duck, Typos, Bitcoin, Kubernetes
 from duckbot.server import Channels, Emojis
 from duckbot.db import Database
 import duckbot.health
@@ -34,7 +35,7 @@ def run_duckbot(bot: commands.Bot):
     bot.load_extension(duckbot.cogs.tito.__name__)
     bot.add_cog(Typos(bot))
     bot.load_extension(duckbot.cogs.recipe.__name__)
-    bot.add_cog(Fortune(bot))
+    bot.load_extension(duckbot.cogs.fortune.__name__)
     bot.load_extension(duckbot.cogs.weather.__name__)
     bot.add_cog(Bitcoin(bot))
     bot.load_extension(duckbot.cogs.insights.__name__)
--- duckbot/cogs/__init__.py
+++ duckbot/cogs/__init__.py
@@ -2,4 +2,3 @@ from .bitcoin import Bitcoin
 from .duck import Duck
 from .kubernetes import Kubernetes
 from .typos import Typos
-from .fortune import Fortune
