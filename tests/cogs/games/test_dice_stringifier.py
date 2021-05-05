1d682904219e2ee3f859bf12c404b6dc4f38309d
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2021-05-05 19:31:04 -0400

    remove repeat

--- tests/cogs/games/test_dice_stringifier.py
+++ tests/cogs/games/test_dice_stringifier.py
@@ -3,8 +3,15 @@ import d20
 from duckbot.cogs.games.dice import DiceStringifier
 
 
-@pytest.mark.repeat(100)
-def test_str_expression_stringifies_node_roll():
+def test_str_expression_stringifies_node_roll_non_crit():
     result = d20.roll("1d20", stringifier=DiceStringifier())
-    expected = f"1d20 ({result.total})" if result.crit == d20.CritType.NONE else f"1d20 (**{result.total}**)"
-    assert result.result == expected
+    while result.crit != d20.CritType.NONE:
+        result = d20.roll("1d20", stringifier=DiceStringifier())
+    assert result.result == f"1d20 ({result.total})"
+
+
+def test_str_expression_stringifies_node_roll_crit():
+    result = d20.roll("1d20", stringifier=DiceStringifier())
+    while result.crit == d20.CritType.NONE:
+        result = d20.roll("1d20", stringifier=DiceStringifier())
+    assert result.result == f"1d20 (**{result.total}**)"
