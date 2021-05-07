c799f230ce73ff0eec4fa7e1e431a547c4f148c1
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2021-05-06 22:13:39 -0400

    enable nltk download outside of venv

--- scripts/build/install.sh
+++ scripts/build/install.sh
@@ -16,5 +16,5 @@ python -m pip install -r "$(git rev-parse --show-toplevel)/requirements-dev.txt"
 
 # install nltk packages for tests (mocking this stuff is impossible)
 for package in "cmudict"; do
-    python -c "import os; import nltk; nltk.download('$package', download_dir=os.path.join('$VIRTUAL_ENV', 'nltk_data'))"
+    python -c "import os; import nltk; nltk.download('$package', download_dir=None if os.getenv('VIRTUAL_ENV') is None else os.path.join('$VIRTUAL_ENV', 'nltk_data'))"
 done
