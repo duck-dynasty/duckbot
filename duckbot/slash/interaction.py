3fed903c3ce78a073144e4bb560468ceb0297ce2
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2021-05-11 17:58:48 -0400

    add interaction model type

---
+++ duckbot/slash/interaction.py
@@ -0,0 +1,19 @@
+from discord import Member, User
+
+
+class Interaction:
+    """https://discord.com/developers/docs/interactions/slash-commands#interaction"""
+
+    def __init__(self, *, bot, data):
+        self.bot = bot
+        self.id = int(data["id"])
+        self.application_id = int(data["application_id"])
+        self.token = data["token"]
+        self.channel = bot.get_channel(data["channel_id"])
+        self.data = data["data"]
+        if "guild_id" in data:
+            self.guild = bot.get_guild(data["guild_id"])
+            self.author = Member(data=data["member"], guild=self.guild, state=bot._connection)
+        else:
+            self.guild = None
+            self.author = User(data=data["user"], state=bot._connection)
