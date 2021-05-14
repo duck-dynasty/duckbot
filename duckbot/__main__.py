03787f42ca1219cdc8a4bfd7238638745783d0db
James Alexander Hughes <jameshughes89@Gmail.com>
2021-05-14 10:24:13 -0300

    configure discord-py logging (#208)

--- duckbot/__main__.py
+++ duckbot/__main__.py
@@ -1,3 +1,5 @@
+import logging
+import logging.handlers
 import os
 from discord import Intents, Game
 from discord.ext import commands
@@ -56,6 +58,19 @@ def intents() -> Intents:
     return intent
 
 
+def logger_setup():
+    log_directory = "logs"
+    os.makedirs(log_directory, exist_ok=True)
+
+    logger = logging.getLogger("discord")
+    logger.setLevel(logging.INFO)
+
+    handler = logging.handlers.RotatingFileHandler(filename=os.path.join(log_directory, "duck.log"), mode="a", maxBytes=256000, backupCount=10)
+    handler.setFormatter(logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s"))
+    logger.addHandler(handler)
+
+
 if __name__ == "__main__":
+    logger_setup()
     bot = commands.Bot(command_prefix="!", help_command=None, intents=intents(), activity=Game(name="Duck Game"))
     run_duckbot(bot)
