62f127f6d3b5f66aeabc84166a9f324af2fc34a4
Taras Mychaskiw <tarasmy@amazon.com>
2021-05-14 11:26:03 -0400

    only run in main, use find_packages

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 from setuptools.command.develop import develop
 from setuptools.command.install import install
 
@@ -25,39 +25,40 @@ class PostInstall(install):
         self.execute(download_nltk_data, [], msg="Download NLTK Data")
 
 
-setup(
-    name="duckbot",
-    version="1.0",
-    url="https://github.com/Chippers255/duckbot",
-    python_requires=">=3.8",
-    packages=[],
-    cmdclass={"develop": PostDevelop, "install": PostInstall},
-    setup_requires=["nltk>=3.6,<4"],
-    install_requires=[
-        "discord.py[voice]>=1.7,<2",
-        "beautifulsoup4",
-        "pytz",
-        "holidays>=0.11,<0.12",
-        "pyowm>=3.2,<4",  # openweather
-        "psycopg2",
-        "sqlalchemy>=1.4,<2",
-        "d20>=1.1.0,<2",
-        "nltk>=3.6,<4",
-    ],
-    extras_require={
-        "dev": [
-            "pytest",
-            "pytest-asyncio",
-            "pytest-mock",
-            "pytest-xdist[psutil]",
-            "pytest-flake8",
-            "pytest-black",
-            "pytest-blockage",
-            "pytest-sugar",
-            "pytest-icdiff",
-            "pytest-cov",
-            "flake8",
-            "black",
-        ]
-    },
-)
+if __name__ == "__main__":
+    setup(
+        name="duckbot",
+        version="1.0",
+        url="https://github.com/Chippers255/duckbot",
+        python_requires=">=3.8",
+        packages=find_packages(),
+        cmdclass={"develop": PostDevelop, "install": PostInstall},
+        setup_requires=["wheel", "nltk>=3.6,<4"],
+        install_requires=[
+            "discord.py[voice]>=1.7,<2",
+            "beautifulsoup4",
+            "pytz",
+            "holidays>=0.11,<0.12",
+            "pyowm>=3.2,<4",  # openweather
+            "psycopg2",
+            "sqlalchemy>=1.4,<2",
+            "d20>=1.1.0,<2",
+            "nltk>=3.6,<4",
+        ],
+        extras_require={
+            "dev": [
+                "pytest",
+                "pytest-asyncio",
+                "pytest-mock",
+                "pytest-xdist[psutil]",
+                "pytest-flake8",
+                "pytest-black",
+                "pytest-blockage",
+                "pytest-sugar",
+                "pytest-icdiff",
+                "pytest-cov",
+                "flake8",
+                "black",
+            ]
+        },
+    )
