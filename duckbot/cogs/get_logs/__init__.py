17b48e0d129fd35b41ed3321fc5bdce55dd5124d
jhughes <jameshughes89@gmail.com>
2021-05-14 12:27:06 -0300

    Create logs command to return an archive of all logs

---
+++ duckbot/cogs/get_logs/__init__.py
@@ -0,0 +1,4 @@
+from .logs import GetLogs
+
+def setup(bot):
+    bot.add_cog(GetLogs(bot))
---
+++ duckbot/cogs/get_logs/logs.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+import os
+import subprocess
+
+class GetLogs(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        # TODO Verify the paths are setup correctly
+        self.duck_dir = os.path.join(os.sep, "duckbot") 
+        self.log_dir = os.path.join(self.duck_dir, "logs") 
+        self.log_archive_path = os.path.join(self.duck_dir, 'logs.tar.gz'),
+        self.compress_command = ['tar'. '-zc', '-f', self.log_archive_path, self.log_dir]
+        
+    @commands.command(name="logs")
+    async def logs_command(self, context):
+        await self.__logs(context)
+
+    async def __logs(self, context):
+        await subprocess.call(self.compress_command)
+        log_archive = discord.File(self.log_archive_path, 'duck.logs')
+        await context.send(file=log_archive)
