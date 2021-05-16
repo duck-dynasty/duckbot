bfd44af1b82179cb2a63c86676e118cf30e457ab
jhughes <jameshughes89@gmail.com>
2021-05-16 19:06:02 -0300

    Finish test_archive_sent & test_archive_remove

--- tests/logging/test_logs.py
+++ tests/logging/test_logs.py
@@ -1,20 +1,17 @@
+import discord
 import os.path
 import pytest
 from duckbot.logging import GetLogs
-
-# TODO: Check the log file is created
-#       Check stderr
-#       Check that stderr matches thing in log
-
-# TODO: This only checks the message string.
-#       Should see if file actually sends
 
 
 @pytest.mark.asyncio
 async def test_archive_sent(bot, context):
     clazz = GetLogs(bot)
     await clazz._GetLogs__logs(context)
-    context.send.assert_called_once_with("duck.logs")
+
+    assert "file" in context.send.call_args.kwargs
+    assert isinstance(context.send.call_args.kwargs["file"], discord.file.File)
+    assert context.send.call_args.kwargs["file"].filename == "duck.logs"
 
 
 @pytest.mark.asyncio
