f3ce208c76d0812f57745ecd1885dd9da0ba75db
jhughes <jameshughes89@gmail.com>
2021-05-17 19:01:21 -0300

    Mocking the tar add (I think)

--- tests/logging/test_get_logs.py
+++ tests/logging/test_get_logs.py
@@ -1,10 +1,16 @@
+from unittest import MagicMock, mock
+
 import pytest
 
 from duckbot.logging import GetLogs
 
 
 @pytest.mark.asyncio
-async def test_get_logs_sends_tarball_of_logs(bot, context):
+@mock.patch("tarfile.open", autospec=True)
+async def test_get_logs_sends_tarball_of_logs(mock_open, bot, context):
+    mock_add = MagicMock()
+    mock_open.return_value.__enter__.return_value.add = mock_add
+
     clazz = GetLogs(bot)
     await clazz._GetLogs__logs(context)
 
