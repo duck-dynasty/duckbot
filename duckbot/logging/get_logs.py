049928ea100cf521a3e736506ab41de0d8ab31c1
jhughes <jameshughes89@gmail.com>
2021-05-19 16:27:39 -0300

    Change absolute path to relative path so the archive doesn't include the whole path.

--- duckbot/logging/get_logs.py
+++ duckbot/logging/get_logs.py
@@ -8,7 +8,7 @@ from discord.ext import commands
 class GetLogs(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.duck_directory = os.getcwd()
+        self.duck_directory = "."
         self.log_directory = os.path.join(self.duck_directory, "logs")
         self.log_archive_path = os.path.join(self.duck_directory, "logs.tar.gz")
 
