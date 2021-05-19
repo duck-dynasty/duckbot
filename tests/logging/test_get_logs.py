51ddb6e11135cd4312e59a07d789bc6712c83360
jhughes <jameshughes89@gmail.com>
2021-05-19 16:50:38 -0300

    Update test_get_logs to (a) be correct and (c) be more thorough.

--- tests/logging/test_get_logs.py
+++ tests/logging/test_get_logs.py
@@ -1,3 +1,4 @@
+import os
 from unittest import mock
 
 import pytest
@@ -8,12 +9,18 @@ from duckbot.logging import GetLogs
 @pytest.mark.asyncio
 @mock.patch("discord.File")
 @mock.patch("tarfile.open")
-async def test_get_logs_sends_tarball_of_logs(tar, discord_file, bot, context):
+@mock.patch("tarfile.TarFile")
+async def test_get_logs_sends_tarball_of_logs(tar, open, file, bot, context):
+    open.return_value = tar
     tar.add = mock.MagicMock()
-    mock_file_id = discord_file.return_value
+    mock_file_id = file.return_value
 
     clazz = GetLogs(bot)
     await clazz._GetLogs__logs(context)
 
+    open.assert_called_once_with(os.path.join(".", "logs.tar.gz"), "w:gz")
+    tar.add.assert_called_once_with(os.path.join(".", "logs"))
+    tar.close.assert_called_once()
+    file.assert_called_once_with(os.path.join(".", "logs.tar.gz"), "logs.tar.gz")
     context.send.assert_called_once()
     assert context.send.call_args.kwargs["file"] == mock_file_id
