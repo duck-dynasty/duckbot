d816878dcafd01a1438ab47e8d3a97824a5abd2c
Taras Mychaskiw <tarasmy@amazon.com>
2021-05-21 13:11:41 -0400

    logs to command_context

--- tests/logging/test_get_logs.py
+++ tests/logging/test_get_logs.py
@@ -9,16 +9,16 @@ from duckbot.logging import GetLogs
 @mock.patch("discord.File")
 @mock.patch("tarfile.open")
 @mock.patch("tarfile.TarFile")
-async def test_get_logs_sends_tarball_of_logs(tar, open, file, bot, context):
+async def test_get_logs_sends_tarball_of_logs(tar, open, file, bot, command_context):
     open.return_value = tar
     mock_file_id = file.return_value
 
     clazz = GetLogs(bot)
-    await clazz._GetLogs__logs(context)
+    await clazz._GetLogs__logs(command_context)
 
     open.assert_called_once_with("logs.tar.gz", "w:gz")
     tar.add.assert_called_once_with("logs")
     tar.close.assert_called_once()
     file.assert_called_once_with("logs.tar.gz", "logs.tar.gz")
-    context.send.assert_called_once()
-    assert context.send.call_args.kwargs["file"] == mock_file_id
+    command_context.send.assert_called_once()
+    assert command_context.send.call_args.kwargs["file"] == mock_file_id
