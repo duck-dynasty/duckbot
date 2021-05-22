fc99f3548f1c996631a55627d60ffe4e56078690
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2021-05-22 13:35:13 -0400

    add duckbot module logs to logs files

--- tests/logging/test_define_logs.py
+++ tests/logging/test_define_logs.py
@@ -10,16 +10,20 @@ from duckbot.logging.define_logs import define_logs
 @pytest.mark.asyncio
 @mock.patch("logging.handlers.RotatingFileHandler")
 @mock.patch("logging.Logger")
+@mock.patch("logging.Logger")
 @mock.patch("logging.getLogger")
 @mock.patch("os.makedirs")
-async def test_define_logs_create_logger(make_dirs, get_logger, logger, handler):
-    get_logger.return_value = logger
+async def test_define_logs_create_logger(make_dirs, get_logger, discord, ducklog, handler):
+    get_logger.side_effect = [discord, ducklog]
     mock_handler_id = handler.return_value
 
     define_logs()
 
     make_dirs.assert_called_once_with("logs", exist_ok=True)
-    get_logger.assert_called_once_with("discord")
-    logger.setLevel.assert_called_once_with(logging.INFO)
     handler.assert_called_once_with(filename=os.path.join("logs", "duck.log"), mode="a", maxBytes=256000, backupCount=10)
-    logger.addHandler.assert_called_once_with(mock_handler_id)
+    get_logger.assert_any_call("discord")
+    discord.setLevel.assert_called_once_with(logging.INFO)
+    discord.addHandler.assert_called_once_with(mock_handler_id)
+    get_logger.assert_any_call("duckbot")
+    ducklog.setLevel.assert_called_once_with(logging.INFO)
+    ducklog.addHandler.assert_called_once_with(mock_handler_id)
