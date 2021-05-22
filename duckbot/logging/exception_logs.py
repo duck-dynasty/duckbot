7b32711fd1054d67ac0dc21012f141a6ea2e5f99
jhughes <jameshughes89@gmail.com>
2021-05-22 11:59:31 -0300

    Fix formatting

--- duckbot/logging/exception_logs.py
+++ duckbot/logging/exception_logs.py
@@ -9,7 +9,6 @@ class ExceptionLogs(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-
     @commands.Cog.listener("on_command_error")
     async def log_exceptions(self, context, exception):
         logger = logging.getLogger("discord")
@@ -17,4 +16,3 @@ class ExceptionLogs(commands.Cog):
         logger.error(exception_string)
         print("Brother, ignoring exception in command {}:".format(context.command), file=sys.stderr)
         traceback.print_exception(type(exception), exception, exception.__traceback__, file=sys.stderr)
-        
