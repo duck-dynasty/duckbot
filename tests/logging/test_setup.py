f3c38c4174a6a90e503eaabdfb94adb67b728c76
James Alexander Hughes <jameshughes89@Gmail.com>
2021-05-22 15:14:19 -0300

    Record command exceptions in logs (#231)

--- tests/logging/test_setup.py
+++ tests/logging/test_setup.py
@@ -1,6 +1,6 @@
 import pytest
 
-from duckbot.logging import GetLogs
+from duckbot.logging import ExceptionLogs, GetLogs
 from duckbot.logging import setup as extension_setup
 from tests.discord_test_ext import assert_cog_added_of_type
 
@@ -9,3 +9,4 @@ from tests.discord_test_ext import assert_cog_added_of_type
 async def test_setup(bot_spy):
     extension_setup(bot_spy)
     assert_cog_added_of_type(bot_spy, GetLogs)
+    assert_cog_added_of_type(bot_spy, ExceptionLogs)
