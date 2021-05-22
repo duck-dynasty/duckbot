492b2a7e34b6c36a48dc0bd08bf2271b6c4b58fb
jhughes <jameshughes89@gmail.com>
2021-05-22 11:54:36 -0300

    Rename function and make decorator listen for on_command_error

--- duckbot/logging/exception_logs.py
+++ duckbot/logging/exception_logs.py
@@ -9,10 +9,12 @@ class ExceptionLogs(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.Cog.listener()
-    async def on_command_error(self, context, exception):
+
+    @commands.Cog.listener("on_command_error")
+    async def log_exceptions(self, context, exception):
         logger = logging.getLogger("discord")
         exception_string = "".join(traceback.format_exception(etype=type(exception), value=exception, tb=exception.__traceback__))
         logger.error(exception_string)
         print("Brother, ignoring exception in command {}:".format(context.command), file=sys.stderr)
         traceback.print_exception(type(exception), exception, exception.__traceback__, file=sys.stderr)
+        
