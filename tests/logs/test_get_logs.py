a0e8f6d3f0bc7d534c849dee2eaa3ed3b8412fb9
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2021-05-25 15:49:13 -0400

    merge command_context

--- tests/logs/test_get_logs.py
+++ tests/logs/test_get_logs.py
@@ -14,7 +14,7 @@ async def test_get_logs_sends_tarball_of_logs(tar, open, file, bot, command_cont
     mock_file_id = file.return_value
 
     clazz = GetLogs(bot)
-    await clazz.logs(context)
+    await clazz.logs(command_context)
 
     open.assert_called_once_with("logs.tar.gz", "w:gz")
     tar.add.assert_called_once_with("logs")
