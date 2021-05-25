0227fe4e1791be16786137d36a900e40a8f8b7b1
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2021-05-25 11:45:57 -0400

    rename logging module to logs to avoid name clash with builtin

--- duckbot/logging/__init__.py
+++
@@ -1,8 +0,0 @@
-from .define_logs import define_logs
-from .exception_logs import ExceptionLogs
-from .get_logs import GetLogs
-
-
-def setup(bot):
-    bot.add_cog(ExceptionLogs(bot))
-    bot.add_cog(GetLogs(bot))
