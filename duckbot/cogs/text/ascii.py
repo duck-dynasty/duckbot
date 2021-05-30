e73fa183781a8535ad22aee748b23c0cd9f245ee
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2021-05-30 18:07:04 -0400

    add to main

--- duckbot/cogs/text/ascii.py
+++ duckbot/cogs/text/ascii.py
@@ -4,3 +4,10 @@ from discord.ext import commands
 class Ascii(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command(name="ascii")
+    async def ascii_command(self, context, *, text: str = None):
+        await self.ascii(context, text)
+
+    async def ascii(self, context, text: str):
+        pass
