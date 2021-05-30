fb5e3c34ad6054504c553b5779ff7a3738ba594a
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2021-05-30 17:49:41 -0400

    add `!coin` to flip a coin

--- tests/cogs/games/test_setup.py
+++ tests/cogs/games/test_setup.py
@@ -1,6 +1,6 @@
 import pytest
 
-from duckbot.cogs.games import AgeOfEmpires, Dice
+from duckbot.cogs.games import AgeOfEmpires, CoinFlip, Dice
 from duckbot.cogs.games import setup as extension_setup
 from tests.discord_test_ext import assert_cog_added_of_type
 
@@ -10,3 +10,4 @@ async def test_setup(bot_spy):
     extension_setup(bot_spy)
     assert_cog_added_of_type(bot_spy, Dice)
     assert_cog_added_of_type(bot_spy, AgeOfEmpires)
+    assert_cog_added_of_type(bot_spy, CoinFlip)
