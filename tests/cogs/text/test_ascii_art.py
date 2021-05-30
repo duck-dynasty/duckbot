c9793fdeea573f78cd0c3c7eddb376443240b100
Taras Mychaskiw <twentylemon@users.noreply.github.com>
2021-05-30 18:28:40 -0400

    Update test_ascii_art.py

--- tests/cogs/text/test_ascii_art.py
+++ tests/cogs/text/test_ascii_art.py
@@ -18,7 +18,7 @@ async def test_ascii_formats_message(figlet, bot, context):
 @mock.patch("pyfiglet.figlet_format", side_effect=["x" * 2000, "happy birthday"])
 async def test_ascii_message_too_long(figlet, bot, context):
     clazz = AsciiArt(bot)
-    await clazz.ascii(context, "some text")
+    await clazz.ascii(context, "it text")
     context.send.assert_called_once_with("Discord won't let me send art with that much GUSTO, so here's a happy birthday.\n```happy birthday```")
-    figlet.assert_any_call("some text")
+    figlet.assert_any_call("it text")
     figlet.assert_any_call("Happy Birthday!")
