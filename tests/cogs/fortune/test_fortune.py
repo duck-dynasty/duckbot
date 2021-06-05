6b41850ec51570cabb4a272d75c12501df7a9979
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2021-06-05 15:34:44 -0400

    add max message length constant

--- tests/cogs/fortune/test_fortune.py
+++ tests/cogs/fortune/test_fortune.py
@@ -2,6 +2,7 @@ from subprocess import CompletedProcess
 from unittest import mock
 
 from duckbot.cogs.fortune import Fortune
+from duckbot.util.messages import MAX_MESSAGE_LENGTH
 
 
 @mock.patch("subprocess.run")
@@ -14,7 +15,7 @@ def test_get_fortune_short(run, bot):
 
 @mock.patch("subprocess.run")
 def test_get_fortune_first_is_long(run, bot):
-    long_message = "f" * 1951
+    long_message = "f" * MAX_MESSAGE_LENGTH
     short_message = "f"
     run.side_effect = [CompletedProcess([], 0, long_message.encode()), CompletedProcess([], 0, short_message.encode())]
     clazz = Fortune(bot)
