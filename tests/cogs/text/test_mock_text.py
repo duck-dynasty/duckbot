a920f2eed7219f6ac7a8a7bb05c01f0d9063435f
Taras Mychaskiw <twentylemon@users.noreply.github.com>
2021-06-06 08:00:49 -0400

    use command_context in !mock

--- tests/cogs/text/test_mock_text.py
+++ tests/cogs/text/test_mock_text.py
@@ -4,17 +4,17 @@ from duckbot.cogs.text import MockText
 
 
 @pytest.mark.asyncio
-async def test_mock_text_mocks_message(bot, context):
+async def test_mock_text_mocks_message(bot, command_context):
     clazz = MockText(bot)
-    await clazz.mock_text(context, "some' message% asd")
-    context.send.assert_called_once_with("SoMe' MeSsAgE% aSd")
-    context.message.delete.assert_called()
+    await clazz.mock_text(command_context, "some' message% asd")
+    command_context.send.assert_called_once_with("SoMe' MeSsAgE% aSd")
+    command_context.message.delete.assert_called()
 
 
 @pytest.mark.asyncio
-async def test_mock_text_no_message(bot, context):
+async def test_mock_text_no_message(bot, command_context):
     context.message.author.display_name = "bob"
     clazz = MockText(bot)
-    await clazz.mock_text(context, "")
-    context.send.assert_called_once_with("BoB, bAsEd On ThIs, I sHoUlD mOcK yOu... I nEeD tExT dUdE.")
-    context.message.delete.assert_called()
+    await clazz.mock_text(command_context, "")
+    command_context.send.assert_called_once_with("BoB, bAsEd On ThIs, I sHoUlD mOcK yOu... I nEeD tExT dUdE.")
+    command_context.message.delete.assert_called()
