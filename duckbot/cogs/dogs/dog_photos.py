86c4670f3194b0792b6d19f2813303593b6f00fd
Taras Mychaskiw <twentylemon@users.noreply.github.com>
2021-06-14 11:42:52 -0400

    accept breed parameter in `!dog` to show dog of that breed (#260)

--- duckbot/cogs/dogs/dog_photos.py
+++ duckbot/cogs/dogs/dog_photos.py
@@ -1,5 +1,6 @@
 import json
 import urllib
+from typing import List, Optional
 
 from discord.ext import commands
 
@@ -9,14 +10,39 @@ class DogPhotos(commands.Cog):
         self.bot = bot
 
     @commands.command(name="dog")
-    async def dog_command(self, context):
-        await context.send(self.get_dog_image())
+    async def dog_command(self, context, *, breed: Optional[str] = None):
+        await self.dog(context, breed)
 
-    def get_dog_image(self):
-        req = urllib.request.Request("https://dog.ceo/api/breeds/image/random")
-        req.add_header("Cookie", "euConsent=true")
+    async def dog(self, context, breed: Optional[str]):
+        if breed and breed in self.get_breeds():
+            await context.send(self.get_dog_image(breed))
+        else:
+            await context.send(self.get_dog_image(None))
+
+    def get_dog_image(self, breed: Optional[str] = None) -> str:
+        if breed:
+            if " " in breed:
+                path = f"breed/{'/'.join(reversed(breed.split()))}/images"
+            else:
+                path = f"breed/{breed}/images"
+        else:
+            path = f"breed/{breed.replace(' ', '/')}/images" if breed else "breeds/image"
+        req = urllib.request.Request(f"https://dog.ceo/api/{path}/random")
+        result = json.loads(urllib.request.urlopen(req).read())
+        if result.get("status", "ded") != "success" or not result.get("message", None):
+            raise RuntimeError(f"could not fetch a puppy; breed = {breed}")
+        else:
+            return result.get("message")
+
+    def get_breeds(self) -> List[str]:
+        req = urllib.request.Request("https://dog.ceo/api/breeds/list/all")
         result = json.loads(urllib.request.urlopen(req).read())
         if result.get("status", "ded") != "success" or not result.get("message", None):
             raise RuntimeError("could not fetch a puppy")
         else:
-            return result.get("message")
+            breeds = []
+            for breed, sub_breeds in result.get("message").items():
+                breeds.append(breed)
+                for sub in sub_breeds:
+                    breeds.append(f"{sub} {breed}")
+            return breeds
--- tests/duckmock/urllib/__init__.py
+++ tests/duckmock/urllib/__init__.py
@@ -6,4 +6,7 @@ URLOPEN = "urllib.request.urlopen"
 
 
 def patch_urlopen(html):
-    return mock.patch(URLOPEN, return_value=MockResponse(data=html))
+    if type(html) == list:
+        return mock.patch(URLOPEN, side_effect=[MockResponse(data=x) for x in html])
+    else:
+        return mock.patch(URLOPEN, return_value=MockResponse(data=html))
