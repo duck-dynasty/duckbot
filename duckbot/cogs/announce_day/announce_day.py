b9e8c3b9ff457afab5cd3d8309f3f456fd6490a7
Taras Mychaskiw <twentylemon@users.noreply.github.com>
2021-07-01 11:12:39 -0400

    use LGTM code quality badge on readme (#293)

--- duckbot/cogs/announce_day/announce_day.py
+++ duckbot/cogs/announce_day/announce_day.py
@@ -2,10 +2,10 @@ import datetime
 import logging
 import random
 
-import discord
 import pytz
 from discord import ChannelType
 from discord.ext import commands, tasks
+from discord.utils import get
 
 from .phrases import days, templates
 from .special_days import SpecialDays
@@ -46,7 +46,7 @@ class AnnounceDay(commands.Cog):
 
     async def on_hour(self):
         if self.should_announce_day():
-            channel = discord.utils.get(self.bot.get_all_channels(), guild__name="Friends Chat", name="general", type=ChannelType.text)
+            channel = get(self.bot.get_all_channels(), guild__name="Friends Chat", name="general", type=ChannelType.text)
             if channel:
                 message = self.get_message()
                 await channel.send(message)
