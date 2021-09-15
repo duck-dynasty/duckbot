643ce9563e15b750482ad082104fd3d155304965
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2021-09-15 19:13:24 -0400

    add docs

--- tests/fixtures/autospec.py
+++ tests/fixtures/autospec.py
@@ -9,6 +9,7 @@ class AutoSpec:
         self.cache = {}
 
     def of(self, spec: str):
+        """Returns an autospec'd mock of the class of the given type."""
         if spec not in self.cache:
             with mock.patch(spec, autospec=True) as instance:
                 self.cache[spec] = instance
@@ -17,4 +18,6 @@ class AutoSpec:
 
 @pytest.fixture(scope="session")
 def autospec() -> AutoSpec:
+    """A fixture to cache autospec'd `mock.patch` invocations. Cannot be used with classes that were imported
+    using `from X import Y`, only those like `import X; X.Y`"""
     return AutoSpec()
