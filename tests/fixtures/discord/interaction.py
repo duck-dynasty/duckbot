902ee48a9e2ee3b89c8c0b79afc3f4597ab0c9fa
twentylemon <taras.mychaskiw@gmail.com>
2021-10-03 10:54:48 -0400

    correct author field to user

--- tests/fixtures/discord/interaction.py
+++ tests/fixtures/discord/interaction.py
@@ -9,5 +9,5 @@ def interaction(request, autospec, channel) -> discord.Interaction:
     """Returns a message with nested properties set, for each channel type a message can be sent to."""
     i.channel = channel
     author = "user" if channel.type in [discord.ChannelType.private, discord.ChannelType.group] else "member"
-    i.author = request.getfixturevalue(author)
+    i.user = request.getfixturevalue(author)
     return i
