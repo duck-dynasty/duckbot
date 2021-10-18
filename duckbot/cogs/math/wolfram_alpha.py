19b49e2ce9d56dbd3adddda9c918678a7925bee3
twentylemon <taras.mychaskiw@gmail.com>
2021-10-18 17:10:57 -0400

    wip; truncate strings to avoid embed limitation errors

--- duckbot/cogs/math/wolfram_alpha.py
+++ duckbot/cogs/math/wolfram_alpha.py
@@ -6,6 +6,8 @@ import discord
 import wolframalpha
 from discord.ext import commands
 
+from duckbot.util.embeds import MAX_EMBED_LENGTH
+
 
 class WolframAlpha(commands.Cog):
     def __init__(self, bot):
@@ -28,16 +30,19 @@ class WolframAlpha(commands.Cog):
         embeds = [self.as_embed(pod) for pod in itertools.islice(result.pods, 5)]
         await context.send(f"https://www.wolframalpha.com/input/?i={urllib.parse.quote_plus(query)}", embeds=embeds)
 
+    # see embed limits: https://discord.com/developers/docs/resources/channel#embed-limits
     def as_embed(self, pod) -> discord.Embed:
-        embed = discord.Embed(title=pod.title)
+        embed = discord.Embed(title=pod.title[:256])
         for sub in itertools.islice(pod.subpods, 10):
             if not embed.image and sub.img and sub.img.src:
                 embed.set_image(url=sub.img.src)
             if sub.title:
-                embed.add_field(name=sub.title, value=sub.plaintext)
+                embed.add_field(name=sub.title[:64], value=sub.plaintext[:512])
             else:
                 name = sub.img.title or sub.img.alt
                 value = sub.plaintext
                 if not (name is None or value is None or name == value):
-                    embed.add_field(name=sub.img.title or sub.img.alt, value=sub.plaintext)
+                    embed.add_field(name=sub.img.title[:64] or sub.img.alt[:64], value=sub.plaintext[:512])
+            if len(embed) > MAX_EMBED_LENGTH:
+                embed.remove_field(len(embed.fields) - 1)  # we tried adding it, but we just can't do it, cap'n
         return embed
