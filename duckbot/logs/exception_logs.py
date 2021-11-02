eafa6228f01de022b795ab3d54432c706221b562
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2021-11-02 19:10:41 -0400

    wip logs

--- duckbot/logs/exception_logs.py
+++ duckbot/logs/exception_logs.py
@@ -8,11 +8,26 @@ from discord.ext import commands
 class ExceptionLogs(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        bot.on_error = self.log_error
 
     @commands.Cog.listener("on_command_error")
-    async def log_exceptions(self, context, exception):
-        logger = logging.getLogger("discord")
+    async def log_exceptions(self, context: commands.Context, exception):
+        logger = logging.getLogger("duckbot")
         exception_string = "".join(traceback.format_exception(etype=type(exception), value=exception, tb=exception.__traceback__))
-        logger.error(exception_string)
+        logger.error(f"{self.format_function(context.command.name, context.args, context.kwargs)}\n{exception_string}")
         print(f"Brother, ignoring exception in command {context.command}:", file=sys.stderr)
         traceback.print_exception(type(exception), exception, exception.__traceback__, file=sys.stderr)
+
+    async def log_error(self, event: str, *args, **kwargs):
+        logger = logging.getLogger("duckbot")
+        trace = traceback.format_exc()
+        args_str = ",".join([str(a) for a in args])
+        kwargs_str = ",".join([f"{k}={v}" for k, v in kwargs.items()])
+        logger.error(f"{event}({args_str}, {kwargs_str}\n{trace}")
+        print(f'Brother, ignoring exception in {event}', file=sys.stderr)
+        traceback.print_exc()
+
+    def format_function(self, name, args, kwargs):
+        args_str = ",".join([str(a) for a in args])
+        kwargs_str = ",".join([f"{k}={v}" for k, v in kwargs.items()])
+        return f"{name}({args_str}, {kwargs_str}"
