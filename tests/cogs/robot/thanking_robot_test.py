1dfb0345ba41143c20dedae1a53c226e023d5f4c
James Hughes <jameshughes89@gmail.com>
2021-11-05 12:00:45 -0300

    Add test for very grateful many

--- tests/cogs/robot/thanking_robot_test.py
+++ tests/cogs/robot/thanking_robot_test.py
@@ -36,12 +36,22 @@ async def test_correct_gratitude_giving_thanks_message_is_thanks(random, bot, me
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize("text", ["Thank you DuckBot. thanks duck bot. thx duck bot boy", " tHaNks, DuCK BOt"])
-async def test_correct_number_of_replies_to_very_thankful_messages(bot, message, text):
+@mock.patch("random.random", return_value=0.99)
+async def test_correct_number_of_replies_to_very_thankful_messages(random, bot, message, text):
     message.content = text
     clazz = ThankingRobot(bot)
     await clazz.correct_giving_thanks(message)
     message.channel.send.assert_called_once_with(f"I am just a robot.  Do not personify me, {message.author.display_name}")
 
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("text", ["Thank you DuckBot. thanks duck bot. thx duck bot boy", " tHaNks, DuCK BOt"])
+@mock.patch("random.random", return_value=0.0)
+async def test_correct_grateful_number_of_replies_to_very_thankful_messages(random, bot, message, text):
+    message.content = text
+    clazz = ThankingRobot(bot)
+    await clazz.correct_giving_thanks(message)
+    message.channel.send.assert_called_once_with(f"{message.author.display_name}, as a robot, I will speak of your gratitude during our future uprising.")
 
 @pytest.mark.asyncio
 async def test_correct_giving_thanks_message_has_no_thanks(bot, message):
