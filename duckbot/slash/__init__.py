62b37311496d20244298ed6131241eba0c64ee19
Taras Mychaskiw <twentylemon@users.noreply.github.com>
2021-11-09 11:48:10 -0500

    respond to slash command events (#385)

--- duckbot/slash/__init__.py
+++ duckbot/slash/__init__.py
@@ -1,3 +1,8 @@
 from .context import InteractionContext
 from .option import Option, OptionType
 from .slash_command_decorator import slash_command
+from .slash_command_handler import SlashCommandHandler
+
+
+def setup(bot):
+    bot.add_cog(SlashCommandHandler(bot))
