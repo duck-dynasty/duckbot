807063f705a41c8581d147fe2b0fabe40f9cbbb4
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2021-11-25 18:18:58 -0500

    add fixture tests

--- tests/app_config_test.py
+++ tests/app_config_test.py
@@ -13,3 +13,13 @@ def test_is_production_env_set_to_test(monkeypatch):
 
 def test_is_production_env_not_set():
     assert not AppConfig.is_production()
+
+
+def test_fixture_is_production_prod(app_config):
+    app_config.is_production = True
+    assert AppConfig.is_production()
+
+
+def test_fixture_is_production_not_prod(app_config):
+    app_config.is_production = False
+    assert not AppConfig.is_production()
