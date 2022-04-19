c93a6bf0d0452b37ca32a01cef714d6783882900
James Alexander Hughes <jameshughes89@Gmail.com>
2022-04-19 10:37:51 -0300

    Duckbot corrects the spelling of that guy (#524)

--- duckbot/cogs/corrections/__init__.py
+++ duckbot/cogs/corrections/__init__.py
@@ -1,6 +1,7 @@
 from .bezos import Bezos
 from .bitcoin import Bitcoin
 from .kubernetes import Kubernetes
+from .tarlson import Tarlson
 from .typos import Typos
 
 
@@ -9,3 +10,4 @@ def setup(bot):
     bot.add_cog(Kubernetes(bot))
     bot.add_cog(Typos(bot))
     bot.add_cog(Bezos(bot))
+    bot.add_cog(Tarlson(bot))
---
+++ duckbot/cogs/corrections/tarlson.py
@@ -0,0 +1,17 @@
+from discord.ext import commands
+
+
+class Tarlson(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener("on_message")
+    async def correct_tarlson(self, message):
+        """Corrections for tarlson"""
+
+        if message.author == self.bot.user:
+            return
+
+        if "tucker carlson" in message.content.lower():
+            correction = "I believe it is pronounced cucker tarlson"
+            await message.channel.send(correction)
