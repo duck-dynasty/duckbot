64cf7da0e3369a6c1909a3421215a5ee48c7ec12
Taras Mychaskiw <tarasmy@amazon.com>
2022-07-20 10:13:50 -0400

    💬 change link text in `!lmgt` to be more googly

--- duckbot/cogs/google/let_me_google_that.py
+++ duckbot/cogs/google/let_me_google_that.py
@@ -15,7 +15,8 @@ class LetMeGoogleThat(commands.Cog):
         await self.let_me_google_that(context, query)
 
     async def let_me_google_that(self, context: commands.Context, query: str):
+        text = f"https://google.com/search?q={urllib.parse.quote_plus(query)}"
         link = f"https://letmegooglethat.com/?q={urllib.parse.quote_plus(query)}"
-        embed = discord.Embed().add_field(name=query, value=f"[Google It!]({link})")
+        embed = discord.Embed().add_field(name=query, value=f"[{text}]({link})")
         await context.send(embed=embed)
         await try_delete(context.message)
