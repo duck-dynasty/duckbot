81448ca9ea243bbc6a3157fe068c8b88c533fcc6
Taras Mychaskiw <twentylemon@users.noreply.github.com>
2022-10-02 18:15:18 -0400

    🚀try spot instances in order to save costs but increase memory available
    YOLO

--- .aws/duckdeploy/stack.py
+++ .aws/duckdeploy/stack.py
@@ -86,7 +86,8 @@ class DuckBotStack(core.Stack):
             max_capacity=1,
             desired_capacity=1,
             machine_image=ec2.MachineImage.generic_linux(ami_map={"us-east-1": "ami-0c90bcaed0062d19b"}),  # custom ECS AMI created manually via https://github.com/aws/amazon-ecs-ami
-            instance_type=ec2.InstanceType.of(instance_class=ec2.InstanceClass.T3, instance_size=ec2.InstanceSize.NANO),
+            instance_type=ec2.InstanceType.of(instance_class=ec2.InstanceClass.T3, instance_size=ec2.InstanceSize.MICRO),
+            spot_price="0.0052",  # t3.nano on-demand price
             block_devices=[autoscaling.BlockDevice(device_name="/dev/xvda", volume=autoscaling.BlockDeviceVolume.ebs(volume_size=8, volume_type=autoscaling.EbsDeviceVolumeType.GP3))],
             key_name="duckbot",  # needs to be created manually
             instance_monitoring=autoscaling.Monitoring.BASIC,
