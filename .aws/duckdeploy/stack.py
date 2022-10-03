69a3b4fd6ad76ea27239f2eb111376db520e9599
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2022-10-03 17:00:20 -0400

    resource name host

--- .aws/duckdeploy/stack.py
+++ .aws/duckdeploy/stack.py
@@ -90,7 +90,7 @@ class DuckBotStack(core.Stack):
             key_name="duckbot",  # needs to be created manually
             machine_image=ec2.MachineImage.generic_linux(ami_map={"us-east-1": "ami-0c90bcaed0062d19b"}),  # custom ECS AMI created manually via https://github.com/aws/amazon-ecs-ami
             security_group=ec2.SecurityGroup(self, "HostSecurityGroup", vpc=vpc),
-            role=iam.Role(self, "AsgRole", assumed_by=iam.ServicePrincipal("ec2.amazonaws.com")),
+            role=iam.Role(self, "HostRole", assumed_by=iam.ServicePrincipal("ec2.amazonaws.com")),
             user_data=ec2.UserData.for_linux(),
         )
         launch_template.connections.allow_to_default_port(file_system)
