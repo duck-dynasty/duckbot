f72ff3f884dae3468c88e241b92e12eb09123a82
jameshughes89 <jameshughes89@gmail.com>
2023-01-15 19:58:17 -0400

    tests

--- tests/cogs/shrug/shrug_test.py
+++ tests/cogs/shrug/shrug_test.py
@@ -2,3 +2,8 @@ import pytest
 
 from duckbot.cogs.shrug import Shrug
 
+
+async def test_shrug_sends_shrug(bot, context):
+    clazz = Shrug(bot)
+    await clazz.shrug(context)
+    context.send.assert_called_once_with("¯\_(ツ)_/¯")
