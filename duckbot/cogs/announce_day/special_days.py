ccb38f4edc2660fb1308829fe3b4c082a6cec686
jameshughes89 <jameshughes89@gmail.com>
2023-01-15 13:34:59 -0400

    update usage

--- duckbot/cogs/announce_day/special_days.py
+++ duckbot/cogs/announce_day/special_days.py
@@ -6,7 +6,7 @@ import holidays
 from dateutil.relativedelta import FR, SU
 from dateutil.relativedelta import relativedelta as rd
 
-from .datetime import now, timezone
+from duckbot.util.datetime import now, timezone
 
 
 class SpecialDays(holidays.Canada):
