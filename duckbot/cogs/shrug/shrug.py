0ec2daf711b5091f5faba1e630c49630db5bf1b6
jameshughes89 <jameshughes89@gmail.com>
2023-01-15 19:56:59 -0400

    rename and 2 methods

--- duckbot/cogs/shrug/shrug.py
+++ duckbot/cogs/shrug/shrug.py
@@ -9,6 +9,9 @@ class Shrug(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="shrug")
-    async def day_command(self, context):
+    @commands.hybrid_command(name="shrug", description="¯\_(ツ)_/¯")
+    async def shrug_command(self, context: commands.Context):
+        await self.shrug(context)
+
+    async def shrug(self, context: commands.Context):
         await context.send("¯\_(ツ)_/¯")
