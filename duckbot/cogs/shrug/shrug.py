aa968574d687984ff042e45a40d3dd67c1bec559
jameshughes89 <jameshughes89@gmail.com>
2023-01-15 20:02:03 -0400

    remove unused

--- duckbot/cogs/shrug/shrug.py
+++ duckbot/cogs/shrug/shrug.py
@@ -1,18 +1,13 @@
-import logging
-import random
-
-from discord import ChannelType
-from discord.ext import commands, tasks
-from discord.utils import get
+from discord.ext import commands
 
 
 class Shrug(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.hybrid_command(name="shrug", description="¯\_(ツ)_/¯")
+    @commands.hybrid_command(name="shrug", description="¯\\_(ツ)_/¯")
     async def shrug_command(self, context: commands.Context):
         await self.shrug(context)
 
     async def shrug(self, context: commands.Context):
-        await context.send("¯\_(ツ)_/¯")
+        await context.send("¯\\_(ツ)_/¯")
--- tests/cogs/shrug/shrug_test.py
+++ tests/cogs/shrug/shrug_test.py
@@ -1,9 +1,7 @@
-import pytest
-
 from duckbot.cogs.shrug import Shrug
 
 
 async def test_shrug_sends_shrug(bot, context):
     clazz = Shrug(bot)
     await clazz.shrug(context)
-    context.send.assert_called_once_with("¯\_(ツ)_/¯")
+    context.send.assert_called_once_with("¯\\_(ツ)_/¯")
