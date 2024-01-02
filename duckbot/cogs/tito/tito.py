b7020559f553de64b4d660bd634e0088660c42ab
Taras Mychaskiw <twentylemon@users.noreply.github.com>
2024-01-02 08:12:10 -0500

    :arrow_up: bump python from 3.8 to 3.10 (#822)

--- duckbot/cogs/tito/tito.py
+++ duckbot/cogs/tito/tito.py
@@ -13,7 +13,7 @@ class Tito(commands.Cog):
     async def react_to_tito_with_yugoslavia(self, message):
         """Adds reactions for all formally Yugoslavian country flags when :tito: is sent."""
         if ":tito:" in message.content:
-            await asyncio.wait([message.add_reaction(f) for f in flags])
+            await asyncio.gather(*[message.add_reaction(f) for f in flags])
 
     @commands.Cog.listener("on_raw_reaction_add")
     async def react_to_tito_reaction(self, payload):
@@ -21,4 +21,4 @@ class Tito(commands.Cog):
         if payload.emoji.name == "tito":
             channel = await self.bot.fetch_channel(payload.channel_id)
             message = await channel.fetch_message(payload.message_id)
-            await asyncio.wait([message.add_reaction(f) for f in flags])
+            await asyncio.gather(*[message.add_reaction(f) for f in flags])
