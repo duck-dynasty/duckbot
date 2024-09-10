7a70f6b667244a78b990195734f3344ebd082246
Taras Mychaskiw <twentylemon@users.noreply.github.com>
2024-09-10 05:38:49 -0400

    ✨ WASHED (#934)

--- duckbot/cogs/formula_one/formula_one.py
+++ duckbot/cogs/formula_one/formula_one.py
@@ -1,5 +1,6 @@
 import random
 
+import discord
 from discord.ext import commands
 
 from .phrases import phrases
@@ -8,11 +9,18 @@ from .phrases import phrases
 class FormulaOne(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.supermax = None
+        self.phrases = [""]
+
+    @commands.Cog.listener("on_ready")
+    async def store_emojis(self):
+        self.supermax = discord.utils.get(self.bot.emojis, guild__name="Friends Chat", name="supermax")
+        self.phrases = phrases(self.supermax)
 
     @commands.Cog.listener("on_message")
     async def car_do_be_going_fast_though(self, message):
         if self.is_dank_channel(message.channel):
-            for letter in random.choice(phrases):
+            for letter in random.choice(self.phrases):
                 await message.add_reaction(letter)
 
     def is_dank_channel(self, channel):
--- tests/cogs/formula_one/formula_one_test.py
+++ tests/cogs/formula_one/formula_one_test.py
@@ -1,16 +1,48 @@
 from unittest import mock
 
+import discord
+import pytest
+
 from duckbot.cogs.formula_one import FormulaOne
 
 
-async def test_car_do_be_going_fast_though_not_dank_channel(bot, message):
+@pytest.fixture
+def supermax_emoji(autospec, guild):
+    guild.name = "Friends Chat"
+    e = autospec.of(discord.Emoji)
+    e.guild = guild
+    e.id = 1
+    e.name = "supermax"
+    e.__str__ = lambda x: "<supermax:1>"
+    return e
+
+
+@pytest.fixture
+def setup_emojis(bot, supermax_emoji):
+    bot.emojis = [supermax_emoji]
+
+
+async def test_store_emojis_emojis_exist(bot, supermax_emoji, setup_emojis):
+    clazz = FormulaOne(bot)
+    await clazz.store_emojis()
+    assert clazz.supermax == supermax_emoji
+
+
+async def test_store_emojis_no_emojis_found(bot):
+    bot.emojis = []
+    clazz = FormulaOne(bot)
+    await clazz.store_emojis()
+    assert clazz.supermax is None
+
+
+async def test_car_do_be_going_fast_though_not_dank_channel(bot, message, setup_emojis):
     clazz = FormulaOne(bot)
     await clazz.car_do_be_going_fast_though(message)
     message.add_reaction.assert_not_called()
 
 
 @mock.patch("random.choice", return_value=["\U0001F170"])
-async def test_car_do_be_going_fast_though_dank_channel(random, bot, message):
+async def test_car_do_be_going_fast_though_dank_channel(random, bot, message, setup_emojis):
     message.channel.name = "dank"
     clazz = FormulaOne(bot)
     await clazz.car_do_be_going_fast_though(message)
