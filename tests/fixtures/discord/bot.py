f7828fa30418f6c6b1b1aa69fef4238937a76a2b
twentylemon <taras.mychaskiw@gmail.com>
2025-07-08 19:37:29 -0400

    🚨 fix some test warnings

--- tests/fixtures/discord/bot.py
+++ tests/fixtures/discord/bot.py
@@ -24,9 +24,13 @@ async def bot(autospec, monkeypatch) -> DuckBot:
     """Returns a mock DuckBot instance. The default event loops are replaced by mocks."""
     b = autospec.of(DuckBot)
     b.command_prefix = "!"
+
+    # stub the bot being started already
+    b.wait_until_ready = mock.Mock(spec=b.wait_until_ready)
+
     b.loop = mock.Mock(spec=asyncio.get_event_loop())
     # mock out loop, it uses `asyncio.get_event_loop()` by default
     monkeypatch.setattr(discord.ext.tasks, "Loop", mock.Mock(spec=discord.ext.tasks.Loop))
     monkeypatch.setattr(discord.ext.tasks, "loop", mock.Mock(spec=discord.ext.tasks.loop))
-    b.wait_until_ready = mock.AsyncMock(spec=discord.Client.wait_until_ready(b))
+
     return b
