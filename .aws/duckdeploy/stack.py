1e2f09d92521f7249147e209b6d0288232ac0700
Taras Mychaskiw <twentylemon@users.noreply.github.com>
2026-02-28 10:06:55 -0500

    🚀 bump cdk to v2 (#1258)

--- .aws/duckdeploy/stack.py
+++ .aws/duckdeploy/stack.py
@@ -1,5 +1,6 @@
 from typing import List
 
+import aws_cdk as cdk
 from aws_cdk import aws_autoscaling as autoscaling
 from aws_cdk import aws_ec2 as ec2
 from aws_cdk import aws_ecs as ecs
@@ -7,13 +8,13 @@ from aws_cdk import aws_efs as efs
 from aws_cdk import aws_iam as iam
 from aws_cdk import aws_logs as logs
 from aws_cdk import aws_ssm as ssm
-from aws_cdk import core
+from constructs import Construct
 
 from .secret import Secret
 
 
-class DuckBotStack(core.Stack):
-    def __init__(self, scope: core.Construct, construct_id: str, *, secrets: List[Secret]):
+class DuckBotStack(cdk.Stack):
+    def __init__(self, scope: Construct, construct_id: str, *, secrets: List[Secret]):
         super().__init__(scope, construct_id)
 
         vpc = ec2.Vpc(
@@ -27,7 +28,7 @@ class DuckBotStack(core.Stack):
         )
 
         postgres_volume_name = "duckbot_dbdata"
-        file_system = efs.FileSystem(self, "PostgresFileSystem", vpc=vpc, encrypted=True, file_system_name=postgres_volume_name, removal_policy=core.RemovalPolicy.DESTROY)
+        file_system = efs.FileSystem(self, "PostgresFileSystem", vpc=vpc, encrypted=True, file_system_name=postgres_volume_name, removal_policy=cdk.RemovalPolicy.DESTROY)
         file_system.node.default_child.override_logical_id("FileSystem")  # rename for compatibility with legacy cloudformation template
 
         task_definition = ecs.TaskDefinition(self, "TaskDefinition", compatibility=ecs.Compatibility.EC2, family="duckbot", network_mode=ecs.NetworkMode.BRIDGE)
@@ -45,10 +46,10 @@ class DuckBotStack(core.Stack):
             },
             health_check=ecs.HealthCheck(
                 command=["CMD", "pg_isready", "-U", "duckbot"],
-                interval=core.Duration.seconds(30),
-                timeout=core.Duration.seconds(5),
+                interval=cdk.Duration.seconds(30),
+                timeout=cdk.Duration.seconds(5),
                 retries=3,
-                start_period=core.Duration.seconds(30),
+                start_period=cdk.Duration.seconds(30),
             ),
             logging=ecs.LogDriver.aws_logs(stream_prefix="ecs", log_retention=logs.RetentionDays.ONE_MONTH),
             memory_reservation_mib=64,
@@ -56,11 +57,7 @@ class DuckBotStack(core.Stack):
         task_definition.add_volume(name=postgres_volume_name, efs_volume_configuration=ecs.EfsVolumeConfiguration(file_system_id=file_system.file_system_id, root_directory="/"))
         postgres.add_mount_points(ecs.MountPoint(source_volume=postgres_volume_name, container_path=postgres_data_path, read_only=False))
 
-        secrets_as_parameters = {
-            # note, parameter version is required by cdk, but does not make it into the template; specify version 1 for simplicity
-            x.environment_name: ssm.StringParameter.from_secure_string_parameter_attributes(self, x.environment_name, parameter_name=x.parameter_name, version=1)
-            for x in secrets
-        }
+        secrets_as_parameters = {x.environment_name: ssm.StringParameter.from_secure_string_parameter_attributes(self, x.environment_name, parameter_name=x.parameter_name) for x in secrets}
         duckbot = task_definition.add_container(
             "duckbot",
             container_name="duckbot",
@@ -70,10 +67,10 @@ class DuckBotStack(core.Stack):
             secrets={k: ecs.Secret.from_ssm_parameter(v) for k, v in secrets_as_parameters.items()},
             health_check=ecs.HealthCheck(
                 command=["CMD", "python", "-m", "duckbot.health"],
-                interval=core.Duration.seconds(30),
-                timeout=core.Duration.seconds(10),
+                interval=cdk.Duration.seconds(30),
+                timeout=cdk.Duration.seconds(10),
                 retries=3,
-                start_period=core.Duration.seconds(30),
+                start_period=cdk.Duration.seconds(30),
             ),
             logging=ecs.LogDriver.aws_logs(stream_prefix="ecs", log_retention=logs.RetentionDays.ONE_MONTH),
             memory_reservation_mib=320,
@@ -86,7 +83,7 @@ class DuckBotStack(core.Stack):
             block_devices=[ec2.BlockDevice(device_name="/dev/xvda", volume=ec2.BlockDeviceVolume.ebs(volume_size=8, volume_type=ec2.EbsDeviceVolumeType.GP3))],
             instance_type=ec2.InstanceType.of(instance_class=ec2.InstanceClass.T3, instance_size=ec2.InstanceSize.MICRO),
             cpu_credits=ec2.CpuCredits.STANDARD,
-            key_name="duckbot",  # needs to be created manually
+            key_pair=ec2.KeyPair.from_key_pair_name(self, "KeyPair", "duckbot"),  # needs to be created manually
             machine_image=ec2.MachineImage.generic_linux(ami_map={"us-east-1": "ami-0cdf40f78318eeff6"}),  # custom ECS AMI created manually via https://github.com/aws/amazon-ecs-ami
             security_group=ec2.SecurityGroup(self, "HostSecurityGroup", vpc=vpc),
             role=iam.Role(self, "HostRole", assumed_by=iam.ServicePrincipal("ec2.amazonaws.com")),
@@ -131,9 +128,9 @@ class DuckBotStack(core.Stack):
                 auto_scaling_group=asg,
                 enable_managed_termination_protection=False,
             ),
-            can_containers_access_instance_role=True,
-            task_drain_time=core.Duration.seconds(0),
         )
+        asg.node.try_remove_child("DrainECSHook")  # suppress automatic drain hook, added by cdk v2
+        asg.node.try_remove_child("LifecycleHookDrainHook")
 
         ecs.Ec2Service(
             self,
